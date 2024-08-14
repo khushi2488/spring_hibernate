9198a87143ada825dcb232833a164f436cd643c7
Khushi5304 <210130107034@gecg28.ac.in>
2024-08-14 16:30:43 +0530

    first commit

---
+++ src/main/java/com/controller/UserController.java
@@ -0,0 +1,35 @@
+
+package com.controller;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.entity.UserEntity;
+import com.repository.UserRepository;
+
+@Controller
+public class UserController {
+
+	@Autowired
+	UserRepository userRepository;
+
+	@GetMapping("/newuser")
+	public String newUser() {
+		return "NewUser";
+	}
+
+	@PostMapping("/saveuser")
+	public String saveUser(UserEntity user) {
+
+		System.out.println(user.getEmail());
+		
+		userRepository.save(user);
+		return "NewUser";
+	}
+
+}
